7ad75ab1d0ee1e3f003b83d058c4df640c80692e
Pervez Choudhury <codedemonuk@gmail.com>
2026-05-16 22:34:43 +0100

    Code coverage with Istanbul

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['tests/**/*.{ts,svelte}'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html']
-    }
-  },
+    plugins: [svelte()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        include: ['tests/**/*.test.{ts,svelte}'],
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'json', 'html'],
+            threshold: {
+                statements: 80,
+                branches: 80,
+                functions: 100,
+                lines: 80
+            }
+        }
+    },
 })
