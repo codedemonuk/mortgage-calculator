1c562df81fe590a4a3881157a80f3617e05503e7
Pervez Choudhury <codedemonuk@gmail.com>
2026-05-16 22:47:54 +0100

    Svelte configuration file added

---
+++ .github/svelte.config.ts
@@ -0,0 +1,14 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import type { Config } from '@sveltejs/kit';
+
+const config: Config = {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        })
+    }
+};
+
+export default config;
